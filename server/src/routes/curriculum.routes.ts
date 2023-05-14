eceffd6c03f12c3a732b6d75baf2b2ad1fab2038
Rubendvb <rubenvasquez556@gmail.com>
2023-05-14 01:06:26 -0300

    criando rotas curriculum

--- server/src/routes/curriculum.routes.ts
+++ server/src/routes/curriculum.routes.ts
@@ -3,7 +3,10 @@ const router = Router();
 
 import * as curriculumController from "../controllers/curriculum.controller";
 
-router.post("/curriculum", curriculumController.createCurriculum);
 router.get("/curriculum", curriculumController.getAllCurriculums);
+router.get("/curriculum/:id", curriculumController.getOneCurriculum);
+router.post("/curriculum", curriculumController.createCurriculum);
+router.put("/curriculum/:id", curriculumController.updateCurriculum);
+router.delete("/curriculum/:id", curriculumController.deleteCurriculum);
 
 export default router;
