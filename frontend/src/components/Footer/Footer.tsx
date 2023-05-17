d645e8e5eae12fc93700524863bed0dada71129c
Rubendvb <rubenvasquez556@gmail.com>
2023-05-16 22:45:11 -0300

    criando rotas Home e Login

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h3>New Beginnings</h3>
+        </div>
+
+        <div>
+          <img src="" alt="Facebook" />
+          <img src="" alt="Twitter" />
+          <img src="" alt="Instagram" />
+        </div>
+      </div>
+      <div>
+        <p>
+          © 2023. Todos direitos reservados a Inclusion Squad. Este material não
+          pode ser publicado, transmitido por broadcast, reescrito ou
+          redistribuído sem autorização.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
+import Login from "./pages/Login/Login.tsx";
+import Header from "./components/Header/Header.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
