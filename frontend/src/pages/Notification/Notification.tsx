34b167b84d898d2e0fef47eef5f3383402ceb5d6
vladoxx <wladimirkochmansky@gmail.com>
2023-05-18 15:51:59 -0300

    Colocanco Classes na tela: Notificações / Notification

--- frontend/src/pages/Notification/Notification.tsx
+++ frontend/src/pages/Notification/Notification.tsx
@@ -1,14 +1,14 @@
 function Notification() {
     return (
-        <div>
-            <h3>Notificação de Email</h3>
-            <p>Mensagens</p>
-            <input type="checkbox"></input>
-            <p>Receber Notificações</p>
-            <input type="checkbox"></input>
-            <p>Não desejo receber emails</p>
+        <div className="notification">
+            <h3 className="notification__tittle">Notificação de Email</h3>
+            <p className="notification__message">Mensagens</p>
+            <input className="notification__input_checkbox" type="checkbox"></input>
+            <p className="notification__alert_on">Receber Notificações</p>
+            <input className="notification__input_checkbox" type="checkbox"></input>
+            <p className="notification__alert_off">Não desejo receber emails</p>
 
-            <button>Salvar</button>
+            <button className="notification__button" type="button">Salvar</button>
         </div>
     )
 }
