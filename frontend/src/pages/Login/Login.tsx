7cbf1a137d6b027b61d43f770dd6c0c29bad004c
vladoxx <wladimirkochmansky@gmail.com>
2023-05-18 13:55:32 -0300

    Colocando Classes na tela de Login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
-      <img src="" alt="Logo Login" />
+    <div className="login">
+      <img className="login__image" src="" alt="Logo Login" />
 
-      <h3>Login</h3>
+      <h3 className="login__tittle">Login</h3>
       
-      <input type="email" placeholder="E-mail/CNPJ" required/>
-      <input type="Password" placeholder="Senha" required/>
+      <input className="login__input_email" type="email" placeholder="E-mail/CNPJ" required/>
+      <input className="login__input_password" type="Password" placeholder="Senha" required/>
       
-      <p>Não tem cadastro? <Link to={"/register"}>Clique aqui</Link></p> 
-      <p>Esqueceu sua senha?</p>
+      <p className="login__register">Não tem cadastro? <Link to={"/register"}>Clique aqui</Link></p> 
+      <p className="login__forgot_password">Esqueceu sua senha?</p>
       
-      <button>Login</button>
+      <button className="login__button" type="button">Login</button>
     </div>
   )
 }
