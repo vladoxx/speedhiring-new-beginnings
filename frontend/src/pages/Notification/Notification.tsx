35cade43e1adeb39fbaee0619fd85eb159dee91c
Carmen Urbina Lopez <81659893+Ofelia-1985@users.noreply.github.com>
2023-05-18 01:45:38 -0300

    criando pages

---
+++ frontend/src/pages/Notification/Notification.tsx
@@ -0,0 +1,16 @@
+function Notification() {
+    return (
+        <div>
+            <h3>Notificação de Email</h3>
+            <p>Mensagens</p>
+            <input type="checkbox"></input>
+            <p>Receber Notificações</p>
+            <input type="checkbox"></input>
+            <p>Não desejo receber emails</p>
+
+            <button>Salvar</button>
+        </div>
+    )
+}
+
+export default Notification
