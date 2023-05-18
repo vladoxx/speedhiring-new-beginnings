ecc693b559a0aace410d8c1d7f374be697348451
vladoxx <wladimirkochmansky@gmail.com>
2023-05-18 14:19:49 -0300

    Colocando Classes na tela de: Minhas Candidaturas

--- frontend/src/pages/Candidatures/Candidatures.tsx
+++ frontend/src/pages/Candidatures/Candidatures.tsx
@@ -1,19 +1,19 @@
 function Candidatures() {
     return (
-        <div>
-            <h3>Minhas Candidaturas</h3>
-            <div>
-                <h4>Atendente de Caixa</h4>
-                <p>Rio de Janeiro</p>
-                <p>Empresa Lorem</p>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        <div className="candidatures">
+            <h3 className="candidatures__tittle">Minhas Candidaturas</h3>
+            <div className="candidatures__box_container">
+                <h4 className="candidatures__role">Atendente de Caixa</h4>
+                <p className="candidatures__city">Rio de Janeiro</p>
+                <p className="candidatures__company">Empresa Lorem</p>
+                <p className="candidatures__job_description">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                     Ullam dolores autem, alias vel earum veniam minus quos delectus
                     excepturi obcaecati eos nesciunt deserunt, in dicta consequatur
                     repellat corporis eaque corrupti.</p>
-                <button>Ver vaga</button>
+                <button className="candidatures__button_job" type="button">Ver vaga</button>
             </div>
 
-            <button>Voltar ao Início</button>
+            <button className="candidatures__button_home" type="button">Voltar ao Início</button>
         </div>
     )
 }
