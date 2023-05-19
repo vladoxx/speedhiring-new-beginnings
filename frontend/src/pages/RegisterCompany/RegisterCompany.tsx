ce3689dad1341c34ae647b30c070ae9a21c992ab
Carmen Urbina Lopez <81659893+Ofelia-1985@users.noreply.github.com>
2023-05-19 12:46:29 -0300

    add form e button

--- frontend/src/pages/RegisterCompany/RegisterCompany.tsx
+++ frontend/src/pages/RegisterCompany/RegisterCompany.tsx
@@ -1,17 +1,19 @@
 function RegisterCompany() {
   return (
-    <div className="register-company">
-       <h3 className="register-company__tittle">Cadastro da Empresa</h3>
-      <input className="register-company__input" type="text" placeholder="Razão Social*" required/>
-      <input className="register-company__input" type="text" placeholder="Sector*" required/>
-      <input className="register-company__input" type="text" placeholder="Endereço*" required/>
-      <input className="register-company__input" type="text" placeholder="CNPJ*" required/>
-      <input className="register-company__input" type="tel" placeholder="Telefone*" required/>
-      <input className="register-company__input" type="text" placeholder="Website" required/>
-      <input className="register-company__input" type="password" placeholder="Senha*" required/>/
-      <input className="register-company__input" type="password" placeholder="Confirmar a Senha*" required/>
-      <button className="register-company__button" type="button">Cadastrar</button>
-    </div>
+    <form action="">
+      <div className="register-company">
+         <h3 className="register-company__tittle">Cadastro da Empresa</h3>
+        <input className="register-company__input" type="text" placeholder="Razão Social*" required/>
+        <input className="register-company__input" type="text" placeholder="Sector*" required/>
+        <input className="register-company__input" type="text" placeholder="Endereço*" required/>
+        <input className="register-company__input" type="text" placeholder="CNPJ*" required/>
+        <input className="register-company__input" type="tel" placeholder="Telefone*" required/>
+        <input className="register-company__input" type="text" placeholder="Website" required/>
+        <input className="register-company__input" type="password" placeholder="Senha*" required/>/
+        <input className="register-company__input" type="password" placeholder="Confirmar a Senha*" required/>
+        <input className="register-company__button" type="submit" value="Cadastrar"/>
+      </div>
+    </form>
   )
 }
 
--- frontend/src/pages/Vacancy/Vacancy.tsx
+++ frontend/src/pages/Vacancy/Vacancy.tsx
@@ -3,35 +3,43 @@ function Vacancy() {
     <div className="vacant">
         <h3 className="vacant__tittle">Vagas</h3>
 
-        <div className="vacant__box_info">
-            <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
-            <p className="vacant__localization">Rio de Janeiro</p>
-            <p className="vacant__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident distinctio fugiat facere voluptatibus dolore cumque,
-                quia eaque sequi laudantium sapiente aspernatur quod dignissimos
-                incidunt quam. Et a officia architecto adipisci!
-            </p>
-        </div>
-        <div className="vacant__box_info">
-            <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
-            <p className="vacant__localization">Rio de Janeiro</p>
-            <p className="vacant__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident distinctio fugiat facere voluptatibus dolore cumque,
-                quia eaque sequi laudantium sapiente aspernatur quod dignissimos
-                incidunt quam. Et a officia architecto adipisci!
-            </p>
-        </div>
-        <div className="vacant__box_info">
-            <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
-            <p className="vacant__localization">Rio de Janeiro</p>
-            <p className="vacant__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident distinctio fugiat facere voluptatibus dolore cumque,
-                quia eaque sequi laudantium sapiente aspernatur quod dignissimos
-                incidunt quam. Et a officia architecto adipisci!
-            </p>
+        <div className="vacant__container">
+            <div className="vacant__box_info">
+                <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
+                <p className="vacant__localization">Rio de Janeiro</p>
+                <p className="vacant__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident distinctio fugiat facere voluptatibus dolore cumque,
+                    quia eaque sequi laudantium sapiente aspernatur quod dignissimos
+                    incidunt quam. Et a officia architecto adipisci!
+                </p>
+                <button type="submit" value="">Ver Vaga</button>
+
+            </div>
+            <div className="vacant__box_info">
+                <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
+                <p className="vacant__localization">Rio de Janeiro</p>
+                <p className="vacant__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident distinctio fugiat facere voluptatibus dolore cumque,
+                    quia eaque sequi laudantium sapiente aspernatur quod dignissimos
+                    incidunt quam. Et a officia architecto adipisci!
+                </p>
+                <button type="submit" value="">Ver Vaga</button>
+
+            </div>
+            <div className="vacant__box_info">
+                <h3 className="vacant__job_tittle">Promotor de Vendas</h3>
+                <p className="vacant__localization">Rio de Janeiro</p>
+                <p className="vacant__description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Provident distinctio fugiat facere voluptatibus dolore cumque,
+                    quia eaque sequi laudantium sapiente aspernatur quod dignissimos
+                    incidunt quam. Et a officia architecto adipisci!
+                </p>
+                <button type="submit" value="">Ver Vaga</button>
+
+            </div>
         </div>
 
         <button className="vacant" type="button">Voltar ao Inicio</button>
