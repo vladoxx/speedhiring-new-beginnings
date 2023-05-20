db181299f61c3b7d47793d65df870b62c35fb0a4
vladoxx <wladimirkochmansky@gmail.com>
2023-05-19 21:22:17 -0300

    Colocando estilos no componente Header - Tela Desktop

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -18,15 +18,14 @@ function Header() {
                 <span className="line line3"></span>
               </div>
               <ul className="header__ul menu-items">
-                <li>
+                <li id="inicio">
                   <Link to={"/"}>Início</Link>
                 </li>
                 <li>Para empresas</li>
                 <li>Candidato</li>
                 <li>Vagas</li>
-                <li>Benefícios</li>
                 <li>Contato</li>
-                <li>
+                <li id="login">
                   <Link to={"/login"}>Login</Link>
                 </li>
               </ul>
@@ -34,8 +33,7 @@ function Header() {
           </div>
         </nav>
         <div className="header__div">
-          <img className="header__img" src={Logo} />
-
+              <img className="header__img" src={Logo} alt="Logo do Site" />
           <h1 className="header_title">New Beginnings</h1>
         </div>
         <h2 className="header_subtitle">
@@ -44,7 +42,7 @@ function Header() {
         <p className="header__paragraph">
           Ajude imigrantes e refugiados a encontrar empregos e superar as
           barreiras que muitas vezes impedem a integração no mercado de
-          trabalho.
+          trabalho
         </p>
       </header>
     </>
