53f074e67650a2f5593a1cb7ed8ca76a9c4efc6f
Carmen Urbina Lopez <81659893+Ofelia-1985@users.noreply.github.com>
2023-05-20 21:06:49 -0300

    add styles na tela company e criando var de fontes

--- frontend/src/pages/Company/Company.tsx
+++ frontend/src/pages/Company/Company.tsx
@@ -7,15 +7,18 @@ function Company() {
             <h3 className="company__tittle">Perfil da Empresa</h3>
 
             <img className="company__image" src={LogoToti} alt="Logo Empresa" />
-            <p className="company__localization">Matriz: Rio de Janeiro</p>
-            <p className="company__site">Site: www.toti.com.br</p>
-            <p className="company__sector">Sector: Educação</p>
-            <p className="company__email">E-mail: toti@empresa.com</p>
-            <p className="company__cnpj">CNPJ: 69.976.803/0001/82</p>
+            <div className="company__list">
+                <ul>
+                    <li className="company__localization">Matriz: Rio de Janeiro</li>
+                    <li className="company__site">Site: www.toti.com.br</li>
+                    <li className="company__sector">Sector: Educação</li>
+                    <li className="company__email">E-mail: toti@empresa.com</li>
+                    <li className="company__cnpj">CNPJ: 69.976.803/0001/82</li>
+                    <li className="company__description">A toti é a primeira plataforma brasileira de ensino e inculsão de pessoas refugiadas e migrantes no mercado
+                de trabalho de tecnologia.</li>
+                </ul>
+            </div>
 
-            <p className="company__description">A toti é a primeira plataforma brasileira de ensino e inclusão de pessoas refugiadas e migrantes no mercado
-                de trabalho de tecnologia.</p>
-                
             <button className="company__vacancies" type="button">Voltar ás vagas</button>
             <button className="company__edit" type="button">Editar dados</button>
             <button className="company__register" type="button">Cadastrar vaga</button>
