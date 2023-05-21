81608d89981efbed070ae86a2bb86271f26d6a5b
Rubendvb <rubenvasquez556@gmail.com>
2023-05-21 00:27:21 -0300

    finalizando css RegisterUser

--- frontend/src/pages/RegisterCompany/RegisterCompany.tsx
+++ frontend/src/pages/RegisterCompany/RegisterCompany.tsx
@@ -1,32 +1,64 @@
-import React from "react";
-import Input from "../../components/Input/Input";
 import "./RegisterCompany.css";
 
-const RegisterCompany = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Lógica para lidar com o envio do formulário
-  };
-
+function RegisterCompany() {
   return (
     <div className="register-company">
       <h3 className="register-company__title">Cadastro da Empresa</h3>
-      <form className="form" onSubmit={handleSubmit}>
-        <Input placeholder="Razão Social*" type="text" />
-        <Input placeholder="Sector*" type="text" />
-        <Input placeholder="Endereço*" type="text" />
-        <Input placeholder="CNPJ*" type="text" />
-        <Input placeholder="Telefone*" type="tel" />
-        <Input placeholder="Website" type="text" />
-        <Input placeholder="Senha*" type="password" />
-        <Input placeholder="Confirmar a Senha*" type="password" />
-
+      <form className="form" action="">
+        <input
+          className="register-company__input"
+          type="text"
+          placeholder="Razão Social*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="text"
+          placeholder="Sector*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="text"
+          placeholder="Endereço*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="text"
+          placeholder="CNPJ*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="tel"
+          placeholder="Telefone*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="text"
+          placeholder="Website"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="password"
+          placeholder="Senha*"
+          required
+        />
+        <input
+          className="register-company__input"
+          type="password"
+          placeholder="Confirmar a Senha*"
+          required
+        />
         <button className="register-company__button" type="submit">
           Cadastrar
         </button>
       </form>
     </div>
   );
-};
+}
 
 export default RegisterCompany;
--- frontend/src/pages/RegisterUser/RegisterUser.tsx
+++ frontend/src/pages/RegisterUser/RegisterUser.tsx
@@ -2,74 +2,74 @@ import "./RegisterUser.css";
 
 function RegisterUser() {
   return (
-    <div className="register-user">
-      <form action="">
-        <h3 className="register-user__tittle">Cadastro do Candidato</h3>
+    <div className="registerUser">
+      <h3 className="registerUser__title">Cadastro do Candidato</h3>
+
+      <form className="registerUser__form" action="">
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="name"
           placeholder="Nome Completo*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="email"
           placeholder="E-mail*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="text"
           placeholder="Endereço*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="number"
           placeholder="CEP*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="tel"
           placeholder="Telefone*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="text"
           placeholder="Cidade*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="text"
           placeholder="Estado"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="text"
           placeholder="Função/Cargo*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="password"
           placeholder="Senha*"
           required
         />
         <input
-          className="register-user__input"
+          className="registerUser__input"
           type="password"
           placeholder="Confirmar a Senha*"
           required
         />
-        <input
-          className="register-user__button"
-          type="submit"
-          value="Cadastrar"
-        />
+
+        <button className="registerUser__button" type="submit">
+          Cadastrar
+        </button>
       </form>
     </div>
   );
